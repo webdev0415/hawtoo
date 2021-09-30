4d176303f9028593fd1758efc8fa8b3981edc248
igorshevk <igor.shevk@gmail.com>
2021-09-30 08:49:36 -0400

    create an onboarding page for new user

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,75 @@
 <template>
   <main class="flex flex-col flex-grow min-h-screen overflow-hidden">
-
     <section class="relative">
       <div class="max-w-6xl px-4 mx-auto sm:px-6">
         <div class="pt-16 md:pt-28">
           <div class="pb-12 text-center md:pb-16">
-
-            <h1 class="max-w-5xl mx-auto mb-6 text-4xl font-extrabold tracking-tighter dark:text-white md:text-5xl lg:text-6xl leading-tighter">
-              Your <span class="text-yellow-400">FREE</span> personalized dashboard to quickly access all the projects you're <span class="line-through ">aped</span> invested in.
+            <h1
+              class="
+                max-w-5xl
+                mx-auto
+                mb-6
+                text-4xl
+                font-extrabold
+                tracking-tighter
+                dark:text-white
+                md:text-5xl
+                lg:text-6xl
+                leading-tighter
+              "
+            >
+              Your <span class="text-yellow-400">FREE</span> personalized
+              dashboard to quickly access all the projects you're
+              <span class="line-through">aped</span> invested in.
             </h1>
 
             <div class="max-w-4xl mx-auto">
-              <p class="mb-8 text-2xl text-gray-600 dark:text-gray-100 md:text-2xl">
-                <span class="block">Aped 🦍 in on a lot of projects? Having trouble keeping track? We gotchu.</span>
-                <span class="block">Keep info about your investments in one single place. Best of all? it's free</span>
+              <p
+                class="
+                  mb-8
+                  text-2xl text-gray-600
+                  dark:text-gray-100
+                  md:text-2xl
+                "
+              >
+                <span class="block"
+                  >Aped 🦍 in on a lot of projects? Having trouble keeping
+                  track? We gotchu.</span
+                >
+                <span class="block"
+                  >Keep info about your investments in one single place. Best of
+                  all? it's free</span
+                >
               </p>
-              <div class="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center">
-                <AppButton variant="primary" class="w-full mb-2 sm:w-auto sm:mb-0" to="/login">Create a free account</AppButton>
+              <div
+                class="max-w-xs mx-auto sm:max-w-none sm:flex sm:justify-center"
+              >
+                <AppButton
+                  variant="primary"
+                  class="w-full mb-2 sm:w-auto sm:mb-0"
+                  to="/login"
+                  >Create a free account</AppButton
+                >
               </div>
             </div>
           </div>
         </div>
 
-        <div v-if="notEmptyObject(popularProjects)" class="grid items-start max-w-sm gap-6 mx-auto mb-20 md:grid-cols-2 lg:grid-cols-3 md:max-w-2xl lg:max-w-none">
+        <div
+          v-if="notEmptyObject(popularProjects)"
+          class="
+            grid
+            items-start
+            max-w-sm
+            gap-6
+            mx-auto
+            mb-20
+            md:grid-cols-2
+            lg:grid-cols-3
+            md:max-w-2xl
+            lg:max-w-none
+          "
+        >
           <div v-for="project in popularProjects" :key="project.id">
             <ProjectCard :data="project" :has-loaded="true" />
           </div>
@@ -30,7 +77,11 @@
       </div>
     </section>
 
-    <HomeTopProjects class="bg-gray-100 dark:bg-gray-900" :has-loaded="notEmptyObject(topProjects)" :projects="topProjects" />
+    <HomeTopProjects
+      class="bg-gray-100 dark:bg-gray-900"
+      :has-loaded="notEmptyObject(topProjects)"
+      :projects="topProjects"
+    />
   </main>
 </template>
 
@@ -40,6 +91,14 @@ import global from '@/mixins/global'
 export default {
   name: 'Home',
   mixins: [global],
+
+  middleware({ redirect, $auth }) {
+    // check if user is new
+    if ($auth.user && !$auth.user.last_sign_in_at) {
+      redirect('/onboarding')
+    }
+  },
+
   async asyncData({ $supabase, $config, error, $content }) {
     const popularResp = await $supabase
       .from('projects')
@@ -66,14 +125,14 @@ export default {
 
     return {
       popularProjects: popularResp.data,
-      topProjects: topProjectsResp.data
+      topProjects: topProjectsResp.data,
     }
   },
 
   data() {
     return {
       allProjects: [],
-      hasLoaded: false
+      hasLoaded: false,
     }
   },
 
@@ -81,7 +140,7 @@ export default {
     // eslint-disable-next-line no-console
     console.log(this.$auth.user)
     console.log(`✨ Thanks for visiting ${process.env.TITLE}`)
-  }
+  },
 }
 </script>
 
---
+++ pages/onboarding.vue
@@ -0,0 +1,20 @@
+<template>
+  <main class="flex flex-col flex-grow min-h-screen overflow-hidden">
+    <section class="max-w-6xl px-4 mx-auto sm:px-6 pt-8 sm:pt-14 ">
+      <h1 class="dark:text-white mb-5">Onboarding - You are new user.</h1>
+      <p class="dark:text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore tenetur
+        odit repellendus repudiandae? Cum fuga, consequatur eius assumenda
+        provident suscipit magnam facilis commodi, iure natus, ipsum
+        exercitationem cupiditate quia laborum?
+      </p>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'onboarding',
+  layout: 'empty'
+}
+</script>
