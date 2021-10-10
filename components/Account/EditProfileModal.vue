74a5dacd1370a91d1b89bdbc24b1d6820b411de1
Chris Schwartze <ch.schwartze@gmail.com>
2021-10-10 21:27:59 +0200

    Edit profile reactive

--- components/Account/EditProfileModal.vue
+++ components/Account/EditProfileModal.vue
@@ -5,12 +5,12 @@
       <div class="flex flex-col items-center mt-4 mb-5 ">
         <div class="mb-4 avatar ">
           <div class="rounded-full w-14 h-14 ring ring-info ring-offset-base-100 ring-offset-1">
-            <img :src=" userAvatarURL ? userAvatarURL : require('~/assets/images/default-avatar-empty.png')" alt="" />
+            <img :src="user.user_metadata.avatar_url ? user.user_metadata.avatar_url : require('~/assets/images/default-avatar-empty.png')" alt="" />
           </div>
         </div>
         <div class="flex flex-col items-center ml-4 ">
           <div class="relative flex items-center px-3 py-2 bg-white border border-gray-300 rounded-md cursor-pointer hover:bg-gray-50 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-blue-gray-50 focus-within:ring-blue-500">
-            <formulate-input name="avatarTemp" type="file" :label="avatarFieldLabel" upload-behavior="live" validation="mime:image/jpeg,image/png,image/gif" :uploader="handleFileUpload" @change="onFileChange" />
+            <formulate-input id="avatar-upload" name="avatarTemp" type="file" :label="avatarFieldLabel" upload-behavior="live" validation="mime:image/jpeg,image/png,image/gif" :uploader="handleFileUpload" @change="onFileChange" />
           </div>
           <div class="flex flex-col items-center mt-4 text-xs text-center ">
             <span>Recommended size: 256 x 256 pixels (jpg, png or gif)</span>
@@ -20,30 +20,11 @@
       </div>
     </div>
 
-    <!-- <FormulateForm v-slot="{ isLoading }" v-model="formValues" class="submit-profile" @submit="handleEditProfile">
-      <div class="col-span-6">
-        <div class="flex flex-col items-center mt-4 mb-5 ">
-          <div class="mb-4 avatar ">
-            <div class="rounded-full w-14 h-14 ring ring-info ring-offset-base-100 ring-offset-1">
-              <img :src=" userAvatarURL ? userAvatarURL : require('~/assets/images/default-avatar-empty.png')" alt="" />
-            </div>
-          </div>
-          <div class="flex flex-col items-center ml-4 ">
-            <div class="relative flex items-center px-3 py-2 bg-white border border-gray-300 rounded-md cursor-pointer hover:bg-gray-50 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-offset-blue-gray-50 focus-within:ring-blue-500">
-              <formulate-input name="avatarTemp" type="file" :label="avatarFieldLabel" upload-behavior="delayed" validation="mime:image/jpeg,image/png,image/gif" :uploader="handleFileUpload" @change="onFileChange" />
-            </div>
-            <div class="flex flex-col items-center mt-4 text-xs text-center ">
-              <span>Recommended size: 256 x 256 pixels (jpg, png or gif)</span>
-              <span class="text-gray-700">(TIP: Upload a GIF to stand-out ✨)</span>
-            </div>
-          </div>
-        </div>
-      </div>
-
+    <FormulateForm v-slot="{ isLoading }" v-model="formValues" class="submit-profile" @submit="handleEditProfile">
       <FormulateInput type="text" name="display_name" label="Display name" validation="required|min:3" placeholder="Your display name" />
       <FormulateInput type="email" name="email" label="Email" validation="required|email" placeholder="Your email" error-behavior="submit" />
       <FormulateInput :disabled="isLoading" type="submit" :label="isLoading ? 'Please wait...' : 'Update profile'" class="text-right mt-7" />
-    </FormulateForm> -->
+    </FormulateForm>
   </Modal>
 </template>
 
@@ -53,64 +34,70 @@ import { mapGetters, mapMutations } from 'vuex'
 
 export default {
   data: () => ({
+    formValues: () => {},
     userAvatarURL: '',
-    localSignedURL: '',
-    avatarFieldLabel: 'Upload avatar',
-    uploadedAvatarFileName: '',
-    formValues: {}
+    avatarFieldLabel: 'Upload avatar'
   }),
-
   computed: {
     ...mapGetters({
       user: 'auth/user',
       getEditProfileModal: 'general/editProfileModal'
     })
+
+    // displayName: {
+    //   set(val) {
+    //     this.$store.commit('auth/SET_DISPLAY_NAME', val)
+    //   },
+    //   get() {
+    //     return this.$store.getters['auth/user'].public_profile.display_name
+    //   }
+    // },
+    // emailAddress: {
+    //   set(val) {
+    //     this.$store.commit('auth/SET_EMAIL', val)
+    //   },
+    //   get() {
+    //     return this.$store.getters['auth/user'].email
+    //   }
+    // }
   },
   mounted() {
-    if (this.user.user_metadata.avatar_url) {
-      this.userAvatarURL = this.user.user_metadata.avatar_url
-    }
     this.formValues = {
-      email: this.user.email,
-      display_name: this.user.user_metadata.display_name || ''
+      email: this.$store.getters['auth/user'].email,
+      display_name: this.$store.getters['auth/user'].public_profile.display_name
     }
   },
   methods: {
     ...mapMutations({
       toggleEditProfileModal: 'general/TOGGLE_EDIT_PROFILE_MODAL'
     }),
-    async handleEditProfile(data) {
+    async handleEditProfile(formData) {
       try {
-        const formData = data
-        delete formData.avatarTemp
-        const updatedData = {
-          email: formData.email,
-          user_metadata: {
-            full_name: formData['display name'],
-            display_name: formData['display name']
-          }
-        }
-
-        if (this.userAvatarURL) {
-          updatedData.user_metadata.avatar = this.userAvatarURL
-        }
-
-        await this.$supabase.auth.update({
-          data: {
-            full_name: formData.display_name,
-            display_name: formData.display_name,
-            avatar_url: this.localSignedURL
-          }
-        })
-
         await this.$supabase
           .from('profiles')
           .update({
-            display_name: formData['display name'],
-            avatar_url: this.localSignedURL
+            display_name: formData.display_name
           })
-          .match({ id: this.$auth.user.id })
+          .match({ id: this.user.id })
 
+        await this.$supabase.auth
+          .update({
+            data: {
+              email: formData.email,
+              full_name: formData.display_name,
+              display_name: formData.display_name
+            }
+          })
+          .then((res) => {
+            if (res.error) {
+              throw new Error(res.error)
+            }
+          })
+          .catch((error) => {
+            throw new Error(error.message)
+          })
+
+        // location.reload()
         this.$toast.success('Updated profile')
       } catch (e) {
         this.$toast.success('There was an error')
@@ -162,8 +149,6 @@ export default {
             })
             .match({ id: this.user.id })
             .then((res) => {
-              console.log('------- Updated profile table -------')
-              console.log(res)
               if (res.error) {
                 throw new Error(error.message)
               }
@@ -174,10 +159,9 @@ export default {
 
           this.$toast.success('Updated avatar')
 
-          this.localSignedURL = avatarUrlResp.data.publicURL
+          location.reload()
         }
       }
-      this.uploadedAvatarFileName = fieName
     },
     onFileChange(e) {
       const file = e.target.files[0]
@@ -197,7 +181,7 @@ export default {
 ul.formulate-files {
   display: none !important;
 }
-#formulate---3_label {
+#avatar-upload_label {
   margin-bottom: 0 !important;
 }
 </style>
