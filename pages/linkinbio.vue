da651a3bbe5aecf421f0ec9de77b39e8f6913fbc
unknown <igor.shevk@gmail.com>
2021-10-10 09:17:58 -0400

    recoer increment page view counter

--- pages/linkinbio.vue
+++ pages/linkinbio.vue
@@ -9,7 +9,7 @@
 </template>
 
 <script>
-// import axios from 'axios'
+import axios from 'axios'
 import getMeta from '~/utils/get-meta'
 import global from '@/mixins/global'
 // import visitorService from '@/utils/visitorService'
@@ -76,17 +76,18 @@ export default {
       if (!this.$auth.user) return false
       else return this.$auth.user
     }
-  }
+  },
 
-  // async mounted() {
-  //   const slug = this.$route.params.id
-  //   const data = await axios.post('/api/increment_page_view', {
-  //     slug
-  //   })
-  //   if (data.status === 200 && data.statusText === 'OK') {
-  //     console.log('data', data)
-  //   }
-  // }
+  async mounted() {
+    console.log('mount')
+    const slug = this.$route.params.id
+    const data = await axios.post('/api/increment_page_view', {
+      slug
+    })
+    if (data.status === 200 && data.statusText === 'OK') {
+      console.log('data', data)
+    }
+  }
 }
 </script>
 
